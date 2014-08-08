7617e53835eba8bfe6fbe3667872ac27948d2c44
yingevil <874827643@qq.com>
2014-08-08 14:34:41 +0800

    modify build_point_state.m file for some errors

--- matlabTools/build_point_state.m
+++ matlabTools/build_point_state.m
@@ -28,14 +28,15 @@ function build_point_state(vidFile)
     
     % assign every point's bright value with gray_frame gray value 
     for i = startIndex : endIndex
-        for index = 1 : len_mask
-            temp.cdata = read(vid, i);          
-            [rgbframe,~] = frame2im(temp);    
-            gray_frame = rgb2gray(rgbframe); 
-            point = mask_points_global(index); 
-            x = point.x; 
-            y = point.y;   
-            point.brightValue(i) = gray_frame(x, y);                
+        
+        temp.cdata = read(vid, i);          
+        [rgbframe,~] = frame2im(temp);    
+        gray_frame = rgb2gray(rgbframe); 
+        for index = 1 : len_mask 
+            
+            x = mask_points_global(index).x; 
+            y = mask_points_global(index).y;   
+            mask_points_global(index).brightValue(i) = gray_frame(x, y);                
         end
     end                         
 end                     
